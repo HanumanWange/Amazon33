6067fce25672215f57734cfe06812794400c83bd
HanumanWange <apphanuman@gmail.com>
2021-09-26 11:08:17 +0530

    Initial Commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,31 @@
 import logo from './logo.svg';
 import './App.css';
+import Product from './components/Product';
+import data from './data';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='master_container'>
+        <header className=''>
+            <a className='amazon_logo' href='amazon.html'>amazon</a>
+            <div className='sign_cart'>
+                <a className='block' href="cart.html">Cart</a>
+                <a className='block' href='signin.html'>SignIn</a>
+            </div>
+        </header>
+
+        <main className=''>
+        <div className='product_items_container'>
+          {data.products.map(product=> (
+            <Product product = {product}/>))}
+        </div>
+          
+        </main>
+
+        <footer className=''>
+            <p>Developed by Hanuman Wange</p>
+
+        </footer>
     </div>
   );
 }
